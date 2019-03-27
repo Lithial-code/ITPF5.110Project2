0d80bb0122f455a0421b7ebb8787aaea9e3a9822
Lithial <jameshessell@gmail.com>
2019-03-27 23:44:53 +1300

    end of day one

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 var paperWeighting = [20,30,50];
 var paperMarks = [74,60,68];
 var paperMax = [80,80,100];
+var possibleGrades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "E"]
+var gradePercentages = [90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 0]
 //math for the percentage of your paper this assignment is worth
 function mathAssignmentPercentage(weighting,mark,maxMark)
 {
@@ -26,5 +28,29 @@ function mathArrayToNumber(list)
     }
     return total;
 }
+//function that confirms grade and if you pass or fail
+function passOrFail(totalGrade)
+{
+    //mark to bool for pass or fail
+    var pass = totalGrade >50;
+
+    //cycle through all the possibles grades. if total is greater than the number in the 
+    //array then you get that mark.
+    for(var i = 0; i < possibleGrades.length; i++)
+    {
+        if(totalGrade > gradePercentages[i])
+        {
+            return `Your current grade is a ${possibleGrades[i]} and pass equals ${pass}`;
+        }
+    }
+}
+
+
+function createCard()
+{
+    
+}
 //console for testing
-console.log(mathArray(mathPaperTotal()));
+console.log(passOrFail(mathArrayToNumber(mathPaperTotal())));
+
+
